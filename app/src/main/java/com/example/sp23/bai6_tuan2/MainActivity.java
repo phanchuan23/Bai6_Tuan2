f02216e093f96048fad3ea440e1e7ed2d443a33f
SP23 <phanchuan.23@gmail.com>
2018-08-21 12:35:05 +0700

    123

---
+++ app/src/main/java/com/example/sp23/bai6_tuan2/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.sp23.bai6_tuan2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
